ff3d330ef9f6e991d64506a13af4cc69ab1422fd
lidingkorol <799834285@qq.com>
2017-06-09 18:08:35 +0800

    爬取拉勾网数据

--- spider/spider.js
+++ spider/spider.js
@@ -7,27 +7,105 @@ const eventproxy = require('eventproxy');
 
 const superagent = require('superagent');
 
+const fs = require('fs');
+
+const path = require('path');
 
 
-
-function start(targetUrl){
-	superagent.get(targetUrl)
-	.end(function(err,res){
-		
+function start(targetUrl,options){
+	superagent
+		.post(targetUrl)
+		.type('form')
+		.set('Content-Type', 'application/x-www-form-urlencoded')
+		.send(options)
+		.end(function(err,res){
+			
+			if(err) {
+				return false;
+			}
+			
+			if(res.success==false){
+				return  false;
+			}
+			
+			console.log(res.text)
+			
+			let jsData = JSON.parse(res.text)
+			
+			console.log(jsData.content.positionResult.result)
+			
+			let result = jsData.content.positionResult.result
+			
+			result = JSON.stringify(result)
+			
+			fs.appendFileSync(path.join(__dirname,'data.js'),result,function(err){
+	  			
+	  			if(err) throw err;
+	  			
+	  			
+	  		
+	  			console.log(2)
+	  			
+	  			
+	  			
+	  		})
+			
+			
+			
+			
+			
 			var $ = cheerio.load(res.text)
+			
+			var data=[];
 		
-		
-			$('a').each(function (idx, element) {
+			$('.con_list_item').each(function (idx, element) {
 				
-	      		console.log($(element).attr('href'));	
+				console.log($(element).attr('data-salary'))
+				
 	      	
 	  		});
-	})
+	  		fs.writeFile(path.join(__dirname,'data.js'),data,function(err){
+	  			
+	  			if(err) throw err;
+	  			
+	  			console.log(1)
+	  			
+	  		})
+		})
 }
 
 exports.spiderGo = () => {
-	var targetUrl = 'https://www.lagou.com/zhaopin/webqianduan/?labelWords=label';
-	start(targetUrl);
+	
+	var targetUrl = 'https://www.lagou.com/jobs/positionAjax.json?px=default&city=%E6%B7%B1%E5%9C%B3&needAddtionalResult=false';
+	 
+	var options = {
+		first:false,
+		pn:2,
+		kd:'web前端'
+	}
+	
+	var i=2;
+	
+	function timer() {
+		
+		if(i<30) {
+			i++;
+			options.pn=i;	
+			start(targetUrl,options);
+			setTimeout(timer,2000);
+		}
+		else {
+			
+			return;
+			
+		}
+		
+		
+	}
+	
+	timer();
+		
+	
 }
 
 
