566f6560aeeae987a8b781d413b784a63c65053f
lidingkorol <799834285@qq.com>
2017-06-12 15:51:40 +0800

    t

--- spider/spider.js
+++ spider/spider.js
@@ -35,17 +35,27 @@ function start(targetUrl,options,header){
 				
 				console.log('ok')
 				
-				let result = reData.content.positionResult.result
+				let result = reData.content.positionResult.result						
+				
+				for(let i=0;i<result.length;i++) {			
+					
+					console.log(details(result[i].positionId))
+					
+				}
 				
 				result = JSON.stringify(result)
 				
-				fs.appendFileSync(path.join(__dirname,'cs_data.js'),result,function(err){
+				
+				
+				
+				
+				/*fs.appendFileSync(path.join(__dirname,'zzz_data.js'),result,function(err){
 		  			
 		  			if(err) throw err;
 
 		  			console.log(2)
 
-		  		})
+		  		})*/
 			
 			}catch(e){
 				
@@ -87,12 +97,63 @@ function start(targetUrl,options,header){
 		
 }
 
+
+async function details(pId) {
+	
+	
+	let tReferer = 'https://www.lagou.com/jobs/list_web前端?city=深圳&cl=false&fromSearch=true&labelWords=&suginput='
+	
+	tReferer = encodeURI(tReferer)
+	
+	let tHeader = {
+		'Content-Type' : 'application/x-www-form-urlencoded; charset=UTF-8',
+		'User-Agent' : 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36',
+		Referer : tReferer,
+		Accept : 'application/json, text/javascript, */*; q=0.01',
+		'Accept-Encoding' : 'gzip, deflate, br'
+	}
+	
+	let pIdUrl = 'https://www.lagou.com/jobs/' +pId+ '.html';
+	
+	var tHtml;
+	
+	console.log(pIdUrl)
+
+	function htp(pIdUrl) {
+		let tRes;
+		superagent
+			.get(pIdUrl)
+			.set(tHeader)
+			.end(function(err,res){
+							
+				let $ = cheerio.load(res.text,{decodeEntities: false})	
+								
+				$('.job_bt p').each(function (idx, element) {
+					
+					tRes += $(element).html();
+					
+		  		});
+				
+			})	
+		
+		console.log(tRes)
+		
+		return tRes
+	}
+		
+	tHtml = await htp(pIdUrl);	
+	
+	console.log(htp(pIdUrl))
+	
+	return tHtml;
+}
+
+
 exports.spiderGo = () => {
 	
-	var targetUrl = 'https://www.lagou.com/jobs/positionAjax.json?px=new&city=%E9%95%BF%E6%B2%99&needAddtionalResult=false';
+	var targetUrl = 'https://www.lagou.com/jobs/positionAjax.json?px=default&city=深圳&needAddtionalResult=false';
 	
-	// 长沙：https://www.lagou.com/jobs/positionAjax.json?px=new&city=%E9%95%BF%E6%B2%99&needAddtionalResult=false
-	// 深圳：https://www.lagou.com/jobs/positionAjax.json?px=default&city=%E6%B7%B1%E5%9C%B3&needAddtionalResult=false
+	targetUrl = encodeURI(targetUrl);
 	
 	
 	var options = {
@@ -141,7 +202,6 @@ exports.spiderGo = () => {
 			
 			header['User-Agent'] = browser[Math.round(Math.random()*9)]
 			
-			console.log(header['User-Agent'])
 			
 			start(targetUrl,options,header);
 			setTimeout(timer,Math.round((Math.random()*9+1)*1000));
